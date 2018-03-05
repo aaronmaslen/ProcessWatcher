1ff7185fd0b6aa80600420d3bef58ec32880fefd
Aaron Maslen <juilin.sandar@gmail.com>
2018-03-05 14:33:13 +1100

    Added console test app

---
+++ ProcessWatcher/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open ProcessWatcher.Common
+
+[<EntryPoint>]
+let main argv =
+    let watcher = new ProcessWatcher.Common.ProcessWatcher("", true, false)
+    0 // return an integer exit code
