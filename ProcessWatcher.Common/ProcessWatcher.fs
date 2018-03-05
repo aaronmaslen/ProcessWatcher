4028e263cae1e2f6c78337e9a6995b811296e536
Aaron Maslen <juilin.sandar@gmail.com>
2018-03-05 15:46:24 +1100

    fixed and finished initial implementation (including console test app)

--- ProcessWatcher.Common/ProcessWatcher.fs
+++ ProcessWatcher.Common/ProcessWatcher.fs
@@ -7,11 +7,22 @@ open System.Management
 open System.Reactive.Linq
 
 module Wmi =
-    let private wmiEventQuery = new WqlEventQuery("__InstanceOperationEvent", "TargetInstance ISA 'Win32_Process'")
+    let private wmiEventQuery = new WqlEventQuery("__InstanceOperationEvent",
+                                                  TimeSpan(0,0,0,0,100),
+                                                  "TargetInstance ISA 'Win32_Process'")
     let private watcher = new ManagementEventWatcher(wmiEventQuery)
     let WatcherEvent = watcher.EventArrived.Select(fun wea -> wea.NewEvent)
     let CreateEvent = WatcherEvent.Where(fun e -> e.ClassPath.ClassName = "__InstanceCreationEvent")
-    let DeleteEvent = WatcherEvent.Where(fun e -> e.ClassPath.ClassName = "_InstanceDeletionEvent")
+    let DeleteEvent = WatcherEvent.Where(fun e -> e.ClassPath.ClassName = "__InstanceDeletionEvent")
+    let mutable private started = false
+    let Start() =
+        if not started then
+            watcher.Start()
+            started <- true
+    let Stop() =
+        if started then
+            watcher.Stop()
+            started <- false
 
 type ProcessEventType =
 | Start
@@ -19,34 +30,49 @@ type ProcessEventType =
 
 type ProcessWatcher(processPath, start, watchChildren) =
     let processName = Path.GetFileName(processPath)
-    let mutable pids : uint32 list = list.Empty
+    let mutable pids : (uint32 * bool) list = list.Empty
     let processEvent = new Event<_>()
+    let endEvent = new Event<_>()
     let createEvent = Wmi.CreateEvent
                          .Select(fun ce -> ce.["TargetInstance"] :?> ManagementBaseObject)
                          .Where(fun targetInstance ->
+                                    pids.Any(fun (pid, _) -> (targetInstance.["ProcessId"] :?> uint32) = pid) ||
                                     targetInstance.["Name"].ToString() = processName ||
-                                    List.contains (targetInstance.["ProcessId"] :?> uint32) pids ||
                                     if watchChildren then
-                                        List.contains (targetInstance.["ParentProcessId"] :?> uint32) pids
+                                        pids.Any(fun (pid, _) -> (targetInstance.["ParentProcessId"] :?> uint32) = pid)
                                     else false)
     let deleteEvent = Wmi.DeleteEvent
                          .Select(fun ce -> ce.["TargetInstance"] :?> ManagementBaseObject)
-                         .Where(fun targetInstance -> 
-                                    targetInstance.["Name"].ToString() = processName ||
-                                    List.contains (targetInstance.["ProcessId"] :?> uint32) pids)
-    do
-        if start then
-            let p : Process = Process.Start(processName)
-            pids <- List.append pids (List.singleton (uint32 p.Id))
-
+                         .Where(fun targetInstance ->
+                                    pids.Any(fun (pid, _) -> (targetInstance.["ProcessId"] :?> uint32) = pid) ||
+                                    targetInstance.["Name"].ToString() = processName)
+    member __.Start() =
         createEvent.Add(fun targetInstance ->
                             let pid = targetInstance.["ProcessId"] :?> uint32
-                            if not (List.contains pid pids) then
-                                pids <- List.append pids (List.singleton pid))
+                            if not (pids.Any(fun (p, _) -> p = pid)) then
+                                pids <- List.append pids (List.singleton (pid, true)))
+
+        deleteEvent.Add(fun targetInstance ->
+                            let pid = targetInstance.["ProcessId"] :?> uint32
+                            if pids.Any(fun (p, _) -> p = pid) then
+                                pids <- List.append
+                                                (List.where (fun (p, _) -> p <> pid) pids)
+                                                (List.singleton (pid, false)))
 
         createEvent.Add(fun targetInstance -> processEvent.Trigger (Start, targetInstance.["Name"].ToString()))
 
         deleteEvent.Add(fun targetInstance -> processEvent.Trigger (Exit, targetInstance.["Name"].ToString()))
 
+        deleteEvent.Add(fun _ -> if not (pids.Any(fun (_, running) -> running)) then endEvent.Trigger())
+
+        Wmi.Start()
+
+        if start then
+            let p : Process = Process.Start(processPath)
+            pids <- List.append pids (List.singleton ((uint32 p.Id), true))
+
     [<CLIEvent>]
     member __.ProcessEvent = processEvent.Publish
+
+    [<CLIEvent>]
+    member __.AllProcessesEndedEvent = endEvent.Publish
--- ProcessWatcher/Program.fs
+++ ProcessWatcher/Program.fs
@@ -1,10 +1,11 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 
 open ProcessWatcher.Common
 
 [<EntryPoint>]
 let main argv =
-    let watcher = new ProcessWatcher.Common.ProcessWatcher("", true, false)
+    let watcher = new ProcessWatcher(argv.[0], true, true)
+    watcher.ProcessEvent.Add (fun (eventType, name) -> Console.WriteLine("{0} {1}", name, eventType))
+    watcher.Start()
+    Async.AwaitEvent (watcher.AllProcessesEndedEvent) |> Async.RunSynchronously
     0 // return an integer exit code
