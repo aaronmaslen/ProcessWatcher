21ae9e6b995b21b4e9e60b06591c5096621a5609
Aaron Maslen <juilin.sandar@gmail.com>
2018-03-12 11:42:28 +1100

    deleted sln for now (not needed) + minor change

--- ProcessWatcher/Program.fs
+++ ProcessWatcher/Program.fs
@@ -7,5 +7,5 @@ let main argv =
     let watcher = new ProcessWatcher(argv.[0], true, true)
     watcher.ProcessEvent.Add (fun (eventType, pid, name) -> Console.WriteLine("{0} ({1}) {2}", name, pid, eventType))
     watcher.Start()
-    Async.AwaitEvent (watcher.AllProcessesEndedEvent) |> Async.RunSynchronously
+    watcher.AllProcessesEndedEvent |> (Async.AwaitEvent >> Async.RunSynchronously)
     0 // return an integer exit code
