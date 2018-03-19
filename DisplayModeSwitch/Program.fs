6014c8c6e32e533583fec6f8fe323550b2d5c96b
Aaron Maslen <juilin.sandar@gmail.com>
2018-03-20 01:01:46 +1100

    Test setting resolution success

--- DisplayModeSwitch/Program.fs
+++ DisplayModeSwitch/Program.fs
@@ -24,4 +24,27 @@ let main argv =
                     | _ -> false
                 ) do
             printfn "    %s" <| m.ToString()
+
+    Console.ReadKey() |> ignore
+
+    let d = GetDisplayDevices() |> Seq.filter (fun d -> d.Active) |> Seq.head
+
+    let m = GetDisplayModes d |> 
+            Seq.filter (fun m ->
+                match m.Resolution with
+                | Some (x,y) -> (x = 1920u) && (y = 1080u)
+                | _ -> false
+                &&
+                match m.FixedOutput with
+                | Some FixedOutputMode.Default -> true
+                | None -> true
+                | _ -> false) |>
+            Seq.head
+    
+    match (SetDisplayMode d m (Seq.singleton Temporary)) with
+    | Ok _ -> "OK"
+    | Error _ -> "Error"
+    |> printfn "%s"
+
+    Console.ReadKey() |> ignore
     0 // return an integer exit code
